67d1db9277355aa718c3b7d23aff280dd7c49dba
Célien Donzé <celien.donze@hefr.ch>
2024-12-04 14:11:34 +0100

    day 2

--- scripts/day_2.py
+++ scripts/day_2.py
@@ -6,14 +6,37 @@ input_path = Path(__file__).parent.parent / "data/inputs/day_2/input.txt"
 
 lines = input_path.read_text().split("\n")
 
+
+def is_problem_safe(numbers: np.ndarray) -> bool:
+    diff = np.diff(numbers)
+    return all(diff > 0) and all(diff <= 3) or all(diff < 0) and all(diff >= -3)
+
+
 number_safe = 0
 for line in lines:
     if not line:
         continue
     numbers = np.array([int(x) for x in line.split(" ")])
-    diff = np.diff(numbers)
-    if all(diff > 0) and all(diff <= 3) or all(diff < 0) and all(diff >= -3):
+    if is_problem_safe(numbers):
         number_safe += 1
 
 print("number of safe lines:")
+print(number_safe)
+
+number_safe = 0
+for line in lines:
+    if not line:
+        continue
+    numbers = np.array([int(x) for x in line.split(" ")])
+    if is_problem_safe(numbers):
+        number_safe += 1
+        continue
+    inner_number_not_safe = 0
+    for i in range(len(numbers)):
+        smaller_numbers = np.delete(numbers, i)
+        if is_problem_safe(smaller_numbers):
+            number_safe += 1
+            break
+
+print("number of safe lines (part 2):")
 print(number_safe)
