d57ff7f8490d79aea3821018d75c7242be5fcd2c
Célien Donzé <celien.donze@hefr.ch>
2024-12-06 14:48:40 +0100

    day 6

--- scripts/day_6.py
+++ scripts/day_6.py
@@ -1,6 +1,8 @@
+from itertools import product
 from pathlib import Path
 
 import numpy as np
+from tqdm import tqdm
 
 day_6_input = Path("data/inputs/day_6/input.txt").read_text().strip().split("\n")
 
@@ -37,9 +39,10 @@ visited = set()
 for i in range(world_map.shape[0]):
     for j in range(world_map.shape[1]):
         if world_map[i, j] not in ".#udlr":
-            current_pos = np.array([i, j])
+            start_pos = np.array([i, j])
             break
 
+current_pos = start_pos.copy()
 # print(current_pos, world_map[tuple(current_pos)])
 visited.add(tuple(current_pos))
 current_direction = directions[0]
@@ -60,4 +63,42 @@ while True:
     current_pos = next_pos
     visited.add(next_coords)
 
-print(len(visited))
+print(len(visited))  # 4758
+
+
+def can_walk_outside(start_pos: tuple, world_map: np.ndarray) -> bool:
+    current_direction = directions[0]
+    current_pos = np.array(start_pos)
+    i = 0
+    while True:
+        i += 1
+        if i > world_map.shape[0] * world_map.shape[1]:
+            return False
+        next_pos = current_pos + np.array(current_direction)
+        if is_outside(next_pos):
+            break
+        next_coords = tuple(next_pos)
+        next_char = world_map[next_coords]
+
+        while next_char == "#":
+            current_direction = directions[
+                (directions.index(current_direction) + 1) % 4
+            ]
+            next_pos = current_pos + np.array(current_direction)
+            next_coords = tuple(next_pos)
+            next_char = world_map[next_coords]
+            # print("changing direction to", current_direction)
+        # print(next_char)
+        current_pos = next_pos
+    return True
+
+
+count = 0
+for i, j in tqdm(list(product(range(world_map.shape[0]), range(world_map.shape[1])))):
+    new_world_map = world_map.copy()
+    new_world_map[i, j] = "#"
+    if not can_walk_outside(start_pos, new_world_map):
+        count += 1
+
+print("Part 2")
+print(count)  # 1670
